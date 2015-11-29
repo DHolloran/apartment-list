08d9f6129fd79af4a333b865906634036998a813
Dan Holloran <dtholloran@gmail.com>
2015-11-29 17:00:56 -0600

    Removes vuefiy config value

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,6 @@ require( 'laravel-elixir-postcss' );
  |
  */
 
-// elixir.config.js.browserify.transformers.push( {
-//     name : 'vueify'
-// } );
-
 elixir( function( mix ) {
     mix.sass( 'app.scss' );
     mix.browserify( 'app.js' );
